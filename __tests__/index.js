326f4fa4192f83e2fe253e3960c5e198635a9662
Shakhzod Ikromov <aabbcc.double@gmail.com>
2024-11-21 12:18:44 +0200

    Project setup

---
+++ __tests__/index.js
@@ -0,0 +1,25 @@
+const launchApp = () => require('../index')
+
+beforeEach(() => {
+  process.exit = jest.fn(code => {
+    throw new Error(`Process exited with code ${code}`)
+  })
+})
+
+it('exists due to insufficient arguments', () => {
+  expect(launchApp).toThrow()
+})
+
+it('exists due to help flag is provided', () => {
+  [['-h'], ['--help'], ['-w', '/tmp/file.env', '--help'], ['/tmp/file.env', '-h']].forEach(value => {
+    process.argv = ['/usr/bin/node', 'index.js'].concat(value)
+    expect(launchApp).toThrow()
+  })
+})
+
+it('exists due to multiple files are provided without overwrite flag', () => {
+  process.argv = ['/usr/bin/node', 'index.js', '/tmp/file1', '/tmp/file2']
+  expect(launchApp).toThrow()
+})
+
+
---
+++ index.js
@@ -0,0 +1,104 @@
+const fsPromises = require('node:fs/promises')
+
+const args = process.argv.slice(2)
+
+function printHelp () {
+  const help = `sort-env-file
+  Sorts .env files while keeping comments attached to the entries`
+
+  process.stderr.write(help)
+}
+
+if (args.includes('-h') || args.includes('--help')) {
+  printHelp()
+  process.exit(0)
+}
+
+if (args.length === 0) {
+  printHelp()
+  process.exit(1)
+}
+
+let overwriteEnabled = false
+const filePaths = []
+args.forEach((value, index, array) => {
+  if (value === '-w' || value === '--overwrite') {
+    overwriteEnabled = true
+    return
+  }
+
+  filePaths.push(value)
+})
+
+if (!overwriteEnabled && filePaths.length !== 1) {
+  process.stderr.write(`Multiple files can be processed only with -w (--overwrite) flag enabled`)
+  process.exit(2)
+}
+
+async function main () {
+  const openedFiles = await Promise.all(
+    filePaths.map(path => fsPromises.open(path, overwriteEnabled ? 'r+' : 'r'))
+  )
+
+  try {
+    const readFiles = await Promise.all(
+      openedFiles.map(handle => handle.readFile({ encoding: 'utf-8' }))
+    )
+
+    const sortedEnvFiles = await Promise.all(
+      readFiles.map(sortEnvContent)
+    )
+
+    if (overwriteEnabled) {
+      await Promise.all(
+        sortedEnvFiles.map(async (content, index) => {
+          await openedFiles.at(index).truncate(0)
+          await openedFiles.at(index).write(content, 0, 'utf-8')
+        })
+      )
+    } else {
+      sortedEnvFiles.forEach(printSorted)
+    }
+  } finally {
+    await Promise.all(
+      openedFiles.map(handle => handle.close())
+    )
+  }
+}
+
+async function printSorted(content) {
+  process.stdout.write(content)
+}
+
+const NEWLINE = '\n'
+
+async function sortEnvContent (content) {
+  const groupedEnvFiles = content.split(NEWLINE).reduce(
+    (groups, line) => {
+      const trimmedLine = line.trim()
+
+      if (trimmedLine.length === 0) {
+        groups.push('')
+      } else {
+        const previousLine = groups.pop()
+        previousLine === '' ? groups.push(line) : groups.push(previousLine + NEWLINE + line)
+      }
+      return groups
+    },
+    ['']
+  ).filter(value => value !== '')
+
+  groupedEnvFiles.sort((a, b) => {
+    const [varA, varB] = [a, b].map(value => value.split(NEWLINE).filter(line => !line.trim().startsWith('#')).at(-1))
+    if (varA < varB)
+      return -1
+    else if (varA > varB)
+      return 1
+    else
+      return 0
+  })
+
+  return groupedEnvFiles.join(NEWLINE + NEWLINE)
+}
+
+main()
